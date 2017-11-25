8d8a7daae4b77a964c9f1af27edbffb5e37e0ec2
tolga87 <tolga.ceng@gmail.com>
2017-11-25 14:34:17 -0800

    cleaned up app delegate, main.m, prefix header, tests, etc.

--- Turquoise/TQAppDelegate.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface TQAppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
-@end
--- Turquoise/main.m
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "TQAppDelegate.h"
-
-int main(int argc, char * argv[]) {
-  @autoreleasepool {
-      return UIApplicationMain(argc, argv, nil, NSStringFromClass([TQAppDelegate class]));
-  }
-}
